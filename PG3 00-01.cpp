e2b86ced82fabd13cde039c41ab2a796da22bd3a
k024g1027@g.neec.ac.jp <k024g1027@g.neec.ac.jp>
2025-05-09 16:21:15 +0900

    00-01

--- PG3 00-01.cpp
+++ PG3 00-01.cpp
@@ -1,8 +1,33 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main()
-{
-    printf("今日は暑いね!\n");
-    return 0;
-} 
+template <typename Type1, typename Type2>
+
+Type1 add(Type1 a, Type2 b) {
+
+	///関数テンプレート
+	if (b<a) {
+		return b;
+	}
+	if (a<b){
+		return a;
+	}
+}
+
+template <>
+char add<char>(char a, char b) {
+	return printf("数字以外は代入できません\n");
+}
+
+int main() {
+
+
+	//計算と結果出力
+	printf("%d\n", add<int,float>(128, 256.0f));
+	printf("%d\n", add<char,char>(110,110));
+	printf("%lf\n", add<double,double>(3.14159265, 2.7182818281));
+
+	return 0;
+}
+
+
 ///source-charset:utf-8
